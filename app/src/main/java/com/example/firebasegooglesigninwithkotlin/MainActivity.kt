e66abd9fc0f6b3b75da19663702b7200bdefcae8
Ana Beatriz Lisboa <megganbeattrizfrutuoso@gmail.com>
2020-11-14 08:56:29 -0300

    Inicializando algumas variaveis e convertendo o codigo idToken em credencial Firebase
    *Primeiro inicializei as variaveis do GoogleLogin que faltavam e consequentemente crashariam o app kkk.
    
    *adicoinei um progressbar que irá deixar o layout mais intuitivo
    
    *no callback da onActivityResult temos o onjeto account que nos permite já poder pegar o idToken e enviar para o back-end
    
    *dentro do método firebaseAuthWithGoogle() recebemos o idToken do usuario e convertemos em uma credencial do firebase. E é  através dessa credencial que logamos o usuário no firebase.  usamos esse método para logar: signInWithCredential(credential)
    
    *Ao concluir o login com a credencial, verificamos se tem algum user logado com o método fAuth.currentUser() e apartir desse resultados atualizamos a UI (adicionado o nome do usuario á alguma textview por exemplo)

--- app/src/main/java/com/example/firebasegooglesigninwithkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firebasegooglesigninwithkotlin/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.firebasegooglesigninwithkotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import android.widget.Toast
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -12,7 +16,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var signOutButton : Button
     lateinit var wellcomeTextView : TextView
     lateinit var userEmailTextView: TextView
+    lateinit var progressBar: ProgressBar
 
     var RC_SIG_IN : Int = 1
 
@@ -31,8 +39,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initGoogleLogin()
         initViews()
+
+        initGoogleLogin()
 
         signInButton.setOnClickListener {
             signIn()
@@ -48,28 +57,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        progressBar = findViewById(R.id.progressBar)
         signInButton = findViewById(R.id.signInGoogleButton)
         signOutButton = findViewById(R.id.signOutGoogleButton)
         wellcomeTextView = findViewById(R.id.textView_Wellcome)
         userEmailTextView = findViewById(R.id.textView_userEmail)
+
+        progressBar.visibility = View.GONE
+        signOutButton.visibility = View.GONE
+
     }
 
     private fun initGoogleLogin() {
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        signInClient = GoogleSignIn.getClient(this,gso)
+        fAuth = FirebaseAuth.getInstance()
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == RC_SIG_IN){
-            var task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            var account : GoogleSignInAccount = task.getResult(ApiException::class.java)!!
+            val task : Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account : GoogleSignInAccount = task.getResult(ApiException::class.java)!!
+
+        //-TODO: Aqui já podemos pegar o idToken (@account.idToken) para mandar via HTTPS para a API de back-end
 
             firebaseAuthWithGoogle(account.idToken)
         }
     }
 
     private fun firebaseAuthWithGoogle(idToken: String?) {
+        progressBar.visibility = View.VISIBLE
+        val credential :AuthCredential = GoogleAuthProvider.getCredential(idToken,null)
+        fAuth.signInWithCredential(credential).addOnCompleteListener(this){task ->
+            if (task.isSuccessful){
+                val user = fAuth.currentUser
+                updateUI(user)
+            }else{
+                Log.w("firebaseLogException","Falha no login com credencial",task.exception)
+            }
+        }
+    }
 
+    private fun updateUI(user: FirebaseUser?) {
+        progressBar.visibility = View.GONE
+        if (user != null){
+            Toast.makeText(this,"Bemvindo(a) ${user.displayName}",Toast.LENGTH_LONG).show()
+            userEmailTextView.visibility = View.VISIBLE
+            userEmailTextView.text = user.email
+            wellcomeTextView.text = user.displayName
+            wellcomeTextView.visibility = View.VISIBLE
+            signInButton.visibility = View.GONE
+            signOutButton.visibility = View.VISIBLE
+        }else{
+            signInButton.visibility = View.VISIBLE
+            signOutButton.visibility = View.GONE
+            userEmailTextView.visibility = View.GONE
+            wellcomeTextView.visibility = View.GONE
+            userEmailTextView.text = ""
+            wellcomeTextView.text = ""
+        }
 
     }
 
